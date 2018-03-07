bd67485f6fc4ae0ff4267f708f8df1539134620d
Sam <sam.mamane@hotmail.fr>
2018-03-07 19:30:53 +0100

    Recommencer

--- HongKongFullStack/Angular/Week 1/conFusion/src/app/app.module.ts
+++ HongKongFullStack/Angular/Week 1/conFusion/src/app/app.module.ts
@@ -1,24 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from '@angular/material';
-import { FlexLayoutModule } from '@angular/flex-layout';
+
 
 import { AppComponent } from './app.component';
-import 'hammerjs';
-import { MenuComponent } from './menu/menu.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MenuComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    MaterialModule,
-    FlexLayoutModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- HongKongFullStack/Angular/Week 1/conFusion/src/shared/dish.ts
+++
@@ -1,8 +0,0 @@
-export class Dish {
-    name: string;
-    image: string;
-    category: string;
-    label: string;
-    price: string;
-    description: string;
-}
