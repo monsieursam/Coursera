57e1b454faaed86923901a2f373b0cb7663e079e
Sam <sam.mamane@hotmail.fr>
2018-03-07 22:17:38 +0100

    Week 1 Task 1

--- HongKongFullStack/Angular/Week 1/conFusion/src/app/dishdetail/dishdetail.component.spec.ts
+++ HongKongFullStack/Angular/Week 1/conFusion/src/app/dishdetail/dishdetail.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MenuComponent } from './menu.component';
+import { DishdetailComponent } from './dishdetail.component';
 
-describe('MenuComponent', () => {
-  let component: MenuComponent;
-  let fixture: ComponentFixture<MenuComponent>;
+describe('DishdetailComponent', () => {
+  let component: DishdetailComponent;
+  let fixture: ComponentFixture<DishdetailComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MenuComponent ]
+      declarations: [ DishdetailComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(MenuComponent);
+    fixture = TestBed.createComponent(DishdetailComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
